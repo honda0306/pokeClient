fc71de66d8223535a9f4f0b65041a57bb93a01b5
Chris Honda <christopher.honda92@gmail.com>
2019-03-23 22:01:46 -0700

    Can go back in pagination

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
         console.log('data', data)
         this.setState({
           pokemon: data.results,
-          prevData: data.prev,
+          prevData: data.previous,
           nextData: data.next,
         });
       })
